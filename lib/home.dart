5a6244a3883930f6a055efab7d5ff1c62013a04f
christiandarvs <christiandarvin4@gmail.com>
2023-01-07 03:54:28 +0800

    add and edit files

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +8,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column()),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://media1.giphy.com/media/RkDZq0dhhYHhxdFrJB/giphy.gif?cid=ecf05e47qa91rfgmyeqtaplrlox1kmgoisz1ijpm59pm7aig&rid=giphy.gif&ct=g'),
+                      fit: BoxFit.cover)),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText('Hi!',
+                        speed: const Duration(milliseconds: 220),
+                        textStyle: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold)),
+                    TyperAnimatedText('I hope you \'re doing well',
+                        speed: const Duration(milliseconds: 220),
+                        textStyle: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    TyperAnimatedText('Don\'t forget to rest',
+                        speed: const Duration(milliseconds: 220),
+                        textStyle: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    TyperAnimatedText('You did your best, okay?',
+                        speed: const Duration(milliseconds: 220),
+                        textStyle: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    TyperAnimatedText('Take care of yourself <33',
+                        speed: const Duration(milliseconds: 220),
+                        textStyle: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                  ],
+                  repeatForever: true,
+                  pause: const Duration(milliseconds: 500),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
